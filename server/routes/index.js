02abce98eb38d5b942d871f73458ba238e21973e
Michael Xu <michaelx.0277@gmail.com>
2024-10-04 10:50:24 -0400

    fixed controller function names for item routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,9 +6,9 @@ router.get('/', indexController.home);
 
 // Routes for /item
 router.get('/items', indexController.getAllItems); // Get all items
-router.get('/item/:id', indexController.getItem); // Get a specific item by ID
-router.delete('/item/:id', indexController.deleteItem); // Delete a specific item by ID
-router.put('/item/:id', indexController.updateItem); // Update a specific item by ID
+router.get('/item/:id', indexController.getItemById); // Get a specific item by ID
+router.delete('/item/:id', indexController.deleteItemById); // Delete a specific item by ID
+router.put('/item/:id', indexController.updateItemById); // Update a specific item by ID
 router.post('/item', indexController.createItem); // Create a new item
 
 
