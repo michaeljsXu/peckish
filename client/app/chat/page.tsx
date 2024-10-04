5d6162ff8bd845d95eb6a33bb5f7ff328429360f
Natalie Chan <natapoke2@gmail.com>
2024-10-04 11:54:21 -0400

    adding temporary chat function, bot replies every 10s

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+interface Message {
+  type: 'user' | 'bot';
+  text: string;
+}
 
 export default function Page() {
-  const [messages, setMessages] = useState<string[]>([]);
-  const [replies, setReplies] = useState<string[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [input, setInput] = useState<string>('');
+  // const [replies, setReplies] = useState<string[]>([]);
 
   const searchParams = useSearchParams();
 
@@ -14,16 +22,69 @@ export default function Page() {
     const prompt: string =
       searchParams.get('prompt') ||
       "A step-by-step recipe for a flavorful dish that's both easy to make and perfect for a weeknight dinner?";
-    console.log(prompt)
-      setMessages((prevMessges) => [...prevMessges, prompt]);
+    console.log(prompt);
+    setMessages(() => [{ type: 'user', text: prompt }]);
+
+    setTimeout(() => {
+      setInterval(() => {
+        console.log('Bot is responding');
+        // pretend bot is answering
+        const reply = 'chirp chirp';
+        setMessages((prevMessages) => [...prevMessages, { type: 'bot', text: reply }]);
+      }, 10_000);
+    }, 5000);
   }, []);
+
+  useEffect(() => {
+    console.log('Updating messages');
+    console.log('Messages', messages);
+
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('Input changed', event);
+    setInput(event.target.value);
+  };
+
+  const onKeyDownEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      setMessages((prevMessages) => [...prevMessages, { type: 'user', text: input }]);
+      setInput('');
+    }
+  };
 
   return (
     <>
       <div className="h-screen w-screen flex flex-row">
-        <div className="h-full w-full flex flex-col max-w-[60%] justify-between items-center">
-          <div>CHAT STUFF</div>
-          <input type="text" placeholder="Type a message" className="border rounded p-2 w-full" />
+        <div className="h-full w-full flex flex-col max-w-[60%] justify-end items-center">
+          <div className="relative bottom-0 w-full overflow-y-auto">
+            {/* <div className="flex flex-col justify-end"> */}
+              {messages.map((message, index) => (
+                <div key={index} className="p-2">
+                  <div
+                    className={`p-2 rounded-lg ${
+                      message.type === 'bot' ? 'bg-[beige]' : 'bg-[pink]'
+                    }`}
+                  >
+                    <p>{message.text}</p>
+                  </div>
+                </div>
+              ))}
+              {/* Dummy div to ensure we scroll to the bottom */}
+              <div ref={messagesEndRef} />
+            {/* </div> */}
+          </div>
+          <input
+            type="text"
+            onChange={handleInputChange}
+            placeholder="Type a message"
+            className="border rounded p-2 w-full"
+            onKeyDown={onKeyDownEvent}
+            value={input}
+          />
         </div>
         <div className="h-full w-full bg-[beige] max-w-[40%]"></div>
       </div>
