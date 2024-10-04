a923f1b107b920f0c1031df157f6372e94908af1
Natalie Chan <natapoke2@gmail.com>
2024-10-04 17:57:02 -0400

    changes to main page to pass prompt to query params

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,9 +2,15 @@
 
 import { useState, useEffect } from 'react';
 import Navbar from './components/navbar';
+import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  const [mainMessage, setMainMessage] = useState("What's on the menu today?");
   const [userInput, setUserInput] = useState('');
+  const [useAvailable, setUseAvailable] = useState(false);
 
   useEffect(() => {
     console.log('User input:', userInput);
@@ -13,36 +19,49 @@ export default function Home() {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       console.log('User input on Enter:', userInput);
+      handleNavigate();
     }
   };
 
   const handleButtonClick = () => {
     console.log("I'm feeling peckish");
+    handleNavigate("I'm feeling peckish");
+    // suggest me anything
+    // message, useAvailable
+  };
+
+  const handleNavigate = (input?: string) => {
+    const queryParams: { [key: string]: string } = {
+      prompt: input ?? userInput,
+      available: useAvailable ? 'true' : 'false',
+    };
+    router.push('/chat?' + new URLSearchParams(queryParams).toString());
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="h-screen w-screen">
       <Navbar />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Welcome to our app
-        </h1>
-        <div className="flex items-center gap-2">
-          <input
-            type="text"
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-            onKeyDown={handleKeyDown}
-            className="border p-2"
-            placeholder="Type your message here..."
-          />
-          <button onClick={() => console.log('User input on Enter:', userInput)} className="btn-orange">
+      <main className="h-full w-full flex flex-col justify-center items-center gap-6">
+        <h1>{mainMessage}</h1>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+          className="w-[450px]"
+          placeholder="Type your message here..."
+        />
+        <div>
+          <button onClick={handleButtonClick} className="mr-5 btn-orange-outline">
+            I&apos;m feeling peckish
+          </button>
+          <button
+            onClick={() => console.log('User input on Enter:', userInput)}
+            className="btn-orange"
+          >
             Enter
           </button>
         </div>
-        <button onClick={handleButtonClick} className="btn-orange">
-          I&apos;m feeling peckish
-        </button>
       </main>
     </div>
   );
