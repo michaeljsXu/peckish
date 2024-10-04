9cd3f7154248ec9177b81e5d1cd9fc49a983f82e
Natalie Chan <natapoke2@gmail.com>
2024-10-04 10:47:59 -0400

    template for chat page

--- client/app/chat/page.tsx
+++ client/app/chat/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-    return <h1>Hello, Chat page!</h1>
-  }
+  return (
+    <>
+      <div className="h-screen w-screen flex flex-row">
+        <div className="h-full w-full flex flex-col max-w-[60%] justify-between items-center">
+          <div>CHAT STUFF</div>
+          <input type="text" placeholder="Type a message" className="border rounded p-2 w-full" />
+        </div>
+        <div className="h-full w-full bg-[beige] max-w-[40%]"></div>
+      </div>
+    </>
+  );
+}
