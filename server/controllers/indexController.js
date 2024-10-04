8571e34a2ca27120294ac1bbae1123eb0c45cc94
Michael Xu <michaelx.0277@gmail.com>
2024-10-04 10:28:29 -0400

    finalized adding routing and controller for item

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,5 +1,24 @@
-const Item = require('../models/Item');
+const Item = require("../models/Item");
 
 exports.home = (req, res) => {
-    res.send('Hello World');
-  };
+  res.send("Hello World");
+};
+
+exports.getAllItems = async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch items" });
+  }
+};
+
+exports.createItem = async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(400).json({ error: "Failed to create item" });
+  }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,8 @@ const indexController = require('../controllers/indexController');
 
 router.get('/', indexController.home);
 
+// Routes for /item
+router.get('/item', indexController.getAllItems);
+router.post('/item', indexController.createItem);
+
 module.exports = router;
