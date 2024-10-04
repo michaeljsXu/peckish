aac7faf1cd1af290a6ba260f43fbb6558aefc04e
Ryan Ngai <ryanngai21@gmail.com>
2024-10-04 13:08:38 -0400

    Cleaned up index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,20 +3,11 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 const itemRouter = require('./itemRouter');
 const recipeRouter = require('./recipeRouter');
-
+const userRouter = require('./userRouter');
 
 router.use('/item', itemRouter);
 router.use('/recipe', recipeRouter);
+router.use('/user', userRouter);
 router.get('/', indexController.home);
-
-// Routes for /users
-router.use('/user', require('./userRouter'));
-
-// Routes for /item
-router.get('/item', indexController.getAllItems); // Get all items
-router.get('/item/:id', indexController.getItemById); // Get a specific item by ID
-router.delete('/item/:id', indexController.deleteItemById); // Delete a specific item by ID
-router.put('/item/:id', indexController.updateItemById); // Update a specific item by ID
-router.post('/item', indexController.createItem); // Create a new item
 
 module.exports = router;
