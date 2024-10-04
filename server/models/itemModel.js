75891564cf27d13d37c9109d355b4608554a1d42
Michael Xu <michaelx.0277@gmail.com>
2024-10-04 10:09:59 -0400

    added itemModel schema

---
+++ server/models/itemModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  expiry: {
+    type: Date,
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: true
+  },
+  isFrozen: {
+    type: Boolean,
+    required: true
+  },
+  count: {
+    type: String,
+    required: true
+  }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
