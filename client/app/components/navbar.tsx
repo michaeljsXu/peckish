0c34fa05634e48780009044fe2cbc866251d5251
vicliu2001 <116909592+vicliu2001@users.noreply.github.com>
2024-10-04 10:37:01 -0400

    added hamburger button for menue dropdown

--- client/app/components/navbar.tsx
+++ client/app/components/navbar.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
+'use client';
 
-const Navbar: React.FC = () => {
-    return (
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    );
-};
+import { useState } from 'react';
 
-export default Navbar;
+export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="relative">
+      <button
+        onClick={toggleMenu}
+        className="fixed top-4 left-4 p-2 bg-gray-800 text-white rounded"
+      >
+        ☰
+      </button>
+      {isMenuOpen && (
+        <div className="fixed top-14 left-4 bg-white shadow-md rounded p-4">
+          <ul className="flex flex-col gap-2">
+            <li>
+              <a href="#Home" className="text-gray-800">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#Chat" className="text-gray-800">
+                Chat
+              </a>
+            </li>
+            <li>
+              <a href="#Inventory" className="text-gray-800">
+                Inventory
+              </a>
+            </li>
+            <li>
+              <a href="#Recipes" className="text-gray-800">
+                Recipes
+              </a>
+            </li>
+            <li>
+              <a href="#Profile" className="text-gray-800">
+                Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+}
